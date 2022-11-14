0d617d84ba9048161b2e367224c90ea0b2e73364
TAMIL-THENDRAL-M <100483687+TAMIL-THENDRAL-M@users.noreply.github.com>
2022-11-14 22:20:41 +0530

    Add files via upload

---
+++ Implimenting Web Application/Create IBM DB2 And Connect With Python/app.py
@@ -0,0 +1,4 @@
+import ibm_db
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=9938aec0-8105-433e-8bf9-0fbb7e483086.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32459;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=vhx73109;PWD=3mvmLTGi7b5QjcSZ",'','')
+print(conn)
+print("connection successful...")
